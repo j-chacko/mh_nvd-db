5b9e046426959974851d6640fcc3202eb08548c8
James Chacko <james.chacko@gmail.com>
2021-11-19 15:21:25 -0700

    Date as a variable

--- nvd_to_cvs.py
+++ nvd_to_cvs.py
@@ -1,30 +1,34 @@
 import nvdlib
 import pandas as pd
+from datetime import datetime
 
-pubStart_Date = "2021-09-26 00:00"
-pubEnd_Date = "2021-10-25 23:59"
+AssEnd_Date = 2021-10-25
+AssStart_Date = AssEnd_Date - 30
+
+PubStart_Date = AssStart_Date + "00:00"
+PubEnd_Date = AssEnd_Date.strftime("%Y-%m-%d") + "23:59"
 
 # Query the CVE's for the application from NVD DB and export to a CSV file
 def nvd_to_csv(CPE_Name, FileName):
-	r = nvdlib.searchCVE(pubStartDate = pubStart_Date, pubEndDate = pubEnd_Date, cpeName = CPE_Name)
+	r = nvdlib.searchCVE(pubStartDate = PubStart_Date, pubEndDate = PubEnd_Date, cpeName = CPE_Name)
 
 	for eachCVE in r:
 		dictionary = {'CVE ID': eachCVE.id, 'CVE Score': str(eachCVE.score[0]), 'CVE Version': eachCVE.score[1], 'Published Date': eachCVE.publishedDate, 'URL': eachCVE.url}
 
-	df= pd.DataFrame()
+	DF= pd.DataFrame()
 	for x in range(len(r)):
-		df_2 = pd.DataFrame(dictionary, index=[x])
-		df = df.append(df_2, ignore_index=True)
+		DF_2 = pd.DataFrame(dictionary, index=[x])
+		DF = DF.append(DF_2, ignore_index=True)
 
-	df.to_csv(FileName)
+	DF.to_csv(FileName)
 
 # For each CPE that needs to be searched  pass the required parameters to the nvd_to_csv function
 Win10_CPE = "cpe:2.3:o:microsoft:windows_10:*:*:*:*:*:*:*:*"
-Win10_FileName = "2021-10-25_win10.csv"
+Win10_FileName = AssEnd_Date.strftime("%Y-%m-%d") + "_win10.csv"
 nvd_to_csv(Win10_CPE, Win10_FileName)
 
 WinXP_CPE = "cpe:2.3:o:microsoft:windows_xp:*:*:*:*:*:*:*:*"
-WinXP_FileName = "2021-10-25_winXP.csv"
+WinXP_FileName = AssEnd_Date.strftime("%Y-%m-%d") + "_winXP.csv"
 nvd_to_csv(WinXP_CPE, WinXP_FileName)
 
 
