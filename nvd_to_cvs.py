41e084f86902218d6be89a3c17eee010aedfa14a
James Chacko <james.chacko@gmail.com>
2021-11-21 12:10:03 -0700

    Edited the variable name formats to be consistent

--- nvd_to_cvs.py
+++ nvd_to_cvs.py
@@ -4,18 +4,19 @@ from datetime import datetime, timedelta, date
 
 
 # The date has to be imported as a datetime object to be able to subtract days.
-AssStart_Date = date(2021, 9, 26)
-AssEnd_Date = date(2021, 10, 25)
-# AssStart_Date = AssEnd_Date - timedelta(days=30)  # Subtract 30 days
+assStartDate = date(2021, 9, 26)
+assEndDate = date(2021, 10, 25)
+# assStartDate = assEndDate - timedelta(days=30)  # Subtract 30 days
 
 
-PubStart_Date = AssStart_Date.strftime("%Y-%m-%d") + " 00:00"  # Added space in time string
-PubEnd_Date = AssEnd_Date.strftime("%Y-%m-%d") + " 23:59"
+pubStartDate = assStartDate.strftime("%Y-%m-%d") + " 00:00"  # Added space in time string
+pubEndDate = assEndDate.strftime("%Y-%m-%d") + " 23:59"
 
 
 # Query the CVE's for the application from NVD DB and export to a CSV file
-def nvd_to_csv(CPE_Name, FileName):
-    r = nvdlib.searchCVE(pubStartDate=PubStart_Date, pubEndDate=PubEnd_Date, cpeName=CPE_Name)
+def nvdToCSV(cpeName, fileName):
+    r = nvdlib.searchCVE(pubStartDate=assStartDate.strftime("%Y-%m-%d") +" 00:00",
+                         pubEndDate=assEndDate.strftime("%Y-%m-%d") + " 23:59", cpeName=cpeName)
 
     # A place to store all the dictionaries that will be made next
     listOfDicts = []
@@ -27,7 +28,7 @@ def nvd_to_csv(CPE_Name, FileName):
         listOfDicts.append(dictionary)  # Adding the dictionary to the list to store them.
 
     # Open/create the file using the filename variable
-    with open(FileName, 'w', newline='') as file:
+    with open(fileName, 'w', newline='') as file:
 
         # Define the first row / column headers
         headers = [
@@ -48,17 +49,18 @@ def nvd_to_csv(CPE_Name, FileName):
 
 
 # For each CPE that needs to be searched  pass the required parameters to the nvd_to_csv function
-Win10_CPE = "cpe:2.3:o:microsoft:windows_10:*:*:*:*:*:*:*:*"
-Win10_FileName = AssEnd_Date.strftime("%Y-%m-%d") + "_win10.csv"
-nvd_to_csv(Win10_CPE, Win10_FileName)
+win10CPE = "cpe:2.3:o:microsoft:windows_10:*:*:*:*:*:*:*:*"
+win10FileName = assEndDate.strftime("%Y-%m-%d") + "_win10.csv"
+nvdToCSV(win10CPE, win10FileName)
 
-WinXP_CPE = "cpe:2.3:o:microsoft:windows_xp:-:*:*:*:*:*:*:*"
-WinXP_FileName = AssEnd_Date.strftime("%Y-%m-%d") + "_winXP.csv"
-nvd_to_csv(WinXP_CPE, WinXP_FileName)
+winXPCPE = "cpe:2.3:o:microsoft:windows_xp:-:*:*:*:*:*:*:*"
+winXPFileName = assEndDate.strftime("%Y-%m-%d") + "_winXP.csv"
+nvdToCSV(winXPCPE, winXPFileName)
 
-Win2012R2_CPE = "cpe:2.3:o:microsoft:windows_server_2012:r2:*:*:*:*:*:*:*"
-Win2012R2_FileName = AssEnd_Date.strftime("%Y-%m-%d") + "_win2012R2.csv"
-nvd_to_csv(Win2012R2_CPE, Win2012R2_FileName)
+win2012R2CPE = "cpe:2.3:o:microsoft:windows_server_2012:r2:*:*:*:*:*:*:*"
+win2012R2FileName = assEndDate.strftime("%Y-%m-%d") + "_win2012R2.csv"
+nvdToCSV(win2012R2CPE, win2012R2FileName)
+
 
 # REFERENCES
 # - https://nvdlib.com/en/latest/
