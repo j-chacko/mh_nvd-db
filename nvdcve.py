37d13301280fabb57774eb72748c123034409239
James Chacko <james.chacko@gmail.com>
2021-11-22 11:52:25 -0700

    Modified code to accept input file, changed file name and added CVE description

--- nvdcve.py
+++ nvdcve.py
@@ -1,19 +1,20 @@
 import nvdlib
 import csv
+import argparse, os
 from datetime import datetime
 
 
 # Query the CVE's for the application from NVD DB and export to a CSV file
 def nvdToCSV(startDate, endDate, cpeName, fileName):
     r = nvdlib.searchCVE(pubStartDate=startDate.strftime("%Y-%m-%d") +" 00:00",
-                         pubEndDate=endDate.strftime("%Y-%m-%d") + " 23:59", cpeName=cpeName)
+                         pubEndDate=endDate.strftime("%Y-%m-%d") + " 23:59", cpeName=cpeName, cpe_dict=True)
 
     # A place to store all the dictionaries that will be made next
     listOfDicts = []
 
     for eachCVE in r:
-        # Making the dictioanary
-        dictionary = {'CVE ID': eachCVE.id, 'CVE Score': str(eachCVE.score[0]), 'CVE Version': eachCVE.score[1],
+        # Making the dictionary
+        dictionary = {'CVE ID': eachCVE.id, 'CVE Description': eachCVE.cve.description.description_data[0].value, 'CVE Score': str(eachCVE.score[0]), 'CVE Version': eachCVE.score[1],
                       'Published Date': eachCVE.publishedDate, 'URL': eachCVE.url}
         listOfDicts.append(dictionary)  # Adding the dictionary to the list to store them.
 
@@ -23,6 +24,7 @@ def nvdToCSV(startDate, endDate, cpeName, fileName):
         # Define the first row / column headers
         headers = [
             'CVE ID',
+            'CVE Description',
             'CVE Score',
             'CVE Version',
             'Published Date',
@@ -38,9 +40,25 @@ def nvdToCSV(startDate, endDate, cpeName, fileName):
                 writer.writerow(eachDict)  # Write each dictionary to the file as a CSV
 
 
-# Read the variable file with the CPE list
-with open('var.csv', newline='') as csvfile:
-    file = csv.reader(csvfile, delimiter=',')
+def validate_file(f):
+    if not os.path.exists(f):
+        # Argparse uses the ArgumentTypeError to give a rejection message like:
+        # error: argument input: x does not exist
+        raise argparse.ArgumentTypeError("{0} does not exist".format(f))
+    return f
+
+
+inputFile = ''
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Search the NVD DB and return CVE's")
+    parser.add_argument("-i", "--input", dest="inputFile", required=True, type=validate_file,
+                        help="input file", metavar="FILE")
+    args = parser.parse_args()
+    inputFile = args.inputFile
+
+# Read the variable file with the paramters
+with open(inputFile, newline='') as csvFile:
+    file = csv.reader(csvFile, delimiter=',')
     for line in file:
         startDate = datetime.strptime(line[0], "%Y-%m-%d")
         endDate = datetime.strptime(line[1], "%Y-%m-%d")
